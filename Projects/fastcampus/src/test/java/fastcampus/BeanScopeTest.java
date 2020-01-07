0606a5f151684a0414200a5ace465083fad91756
khm0813 <khm0813@modernwave.co.kr>
2020-01-07 17:29:33 +0900

    Chapter 04. 스프링 프레임워크 코어 The IOC Container - Bean Scopes - 1 진행중

---
+++ Projects/fastcampus/src/test/java/fastcampus/BeanScopeTest.java
@@ -0,0 +1,18 @@
+package fastcampus;
+
+
+import org.junit.jupiter.api.Test;
+
+public class BeanScopeTest {
+	@Test
+	public void testIdentity(){
+		//@TODO 동일성 vs 동등성
+		//동일성(Identity) : 객체 주소가 같다. - (obj1 == obj2 == obj3) == true
+		//동등성(equals) : 객체 값이 같다. - obj1.equals(obj2) == true;
+
+
+
+
+	}
+
+}
