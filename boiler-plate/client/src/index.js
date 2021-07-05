8f6326f85b65a680482aebff770d61b8a67a22bc
khm0813@eduvation.co.kr <hyeongmin_kim@daekyo.co.kr>
2021-07-05 19:15:13 +0900

    sass

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware ,createStore } from 'redux';
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={ }
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
