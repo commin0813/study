4236b29b8a4abff384d434b32a6147bdffec6ed4
khm0813@eduvation.co.kr <hyeongmin_kim@daekyo.co.kr>
2021-07-07 19:01:47 +0900

    aa

---
+++ node-bird/front/pages/profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout';
+const Profile = () => {
+    return (
+        <AppLayout><h1>Profile</h1></AppLayout>
+    );
+}
+
+export default Profile
---
+++ node-bird/front/pages/signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout';
+const SignUp = () => {
+    return (
+        <AppLayout><h1>회원가입</h1></AppLayout>
+    );
+}
+
+export default SignUp
